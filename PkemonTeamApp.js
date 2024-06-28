a04e0785696f1ba679e8a367cee2643b0dce0758
Tim Ludwiczak <tim.ludwiczak@gmail.com>
2024-06-28 07:52:59 -0500

    CSS

--- PkemonTeamApp.js
+++ PkemonTeamApp.js
@@ -1,9 +1,6 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 
-import React, { useState } from 'react';
-import axios from 'axios';
-
 const PokemonTeamApp = () => {
   const [pokemonImage, setPokemonImage] = useState(null);
 
@@ -19,6 +16,36 @@ const PokemonTeamApp = () => {
       }
     } catch (error) {
       console.error('Error fetching Pokemon data:', error);
+    }
+  };
+  const fetchPokemonByTypes = async (types) => {
+    try {
+      const promises = types.map(type =>
+        axios.get(`https://pokeapi.co/api/v2/type/${type}`)
+      );
+      const responses = await Promise.all(promises);
+      const allPokemon = responses.flatMap(response => response.data.pokemon);
+      const randomPokemon = [];
+      const uniqueIds = new Set();
+
+      while (randomPokemon.length < 5 && uniqueIds.size < allPokemon.length) {
+        const randomIndex = Math.floor(Math.random() * allPokemon.length);
+        const randomPoke = allPokemon[randomIndex].pokemon;
+        const pokeId = parseInt(randomPoke.url.split('/').slice(-2, -1)[0], 10);
+
+        if (!uniqueIds.has(pokeId)) {
+          uniqueIds.add(pokeId);
+          randomPokemon.push(randomPoke);
+        }
+      }
+      const imagePromises = randomPokemon.map(poke =>
+        axios.get(poke.url)
+      );
+      const imageResponses = await Promise.all(imagePromises);
+      const images = imageResponses.map(response => response.data.sprites.front_default);
+      setTeamImages(images);
+    } catch (error) {
+      console.error('Error fetching Pokemon by types:', error);
     }
   };
 
